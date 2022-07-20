01ced08f32d49ffb6942a9d08121713d34900e28
takahaa <Mametubu0219Mazuru@icloud.com>
2022-07-20 16:36:04 +0900

    class Box の作成 インスタンスの生成

--- Program.cs
+++ Program.cs
@@ -1,13 +1,50 @@
 ﻿using InputUtilitys;
-namespace Ex_2022_Template
+namespace Ex26_19_Takahashi
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var name = InputUtility.Input("Name:");
-            var age = InputUtility.InputNumber("Age:", 120);
-            Console.WriteLine($"Hello {name}! \n U are {age} yeaes old.");
+            Box box = new Box(
+                InputUtility.InputFloat("幅"),
+                InputUtility.InputFloat("高さ"),
+                InputUtility.InputFloat("奥行"));
+
+            Console.WriteLine($"Boxの表面積={box.GetSurface()}\nBoxの体積={box.GetVlueme()}");
+
+            /*
+             * 別バージョン
+            var width = InputUtility.InputFloat("幅");
+            var height = InputUtility.InputFloat("高さ");
+            var depth = InputUtility.InputFloat("奥行");
+            Box box = new Box (width, height, depth);
+            */
+        }
+
+
+    }
+    class Box
+    {
+        float width;
+        float height;
+        float depth;
+
+        public Box(float width, float height, float depth)
+        {
+            this.width = width;
+            this.height = height;
+            this.depth = depth;
+
+        }
+
+        public float GetSurface()
+        {
+            return (width * height + depth * height + width * depth) * 2;
+        }
+
+        public float GetVlueme()
+        {
+            return width * height * depth;
         }
     }
 }
