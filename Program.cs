70a052286a763f19ba31bdd600d1ac72db8140c8
takahaa <Mametubu0219Mazuru@icloud.com>
2022-07-27 13:59:42 +0900

    円柱、球、三角柱の追加

--- Program.cs
+++ Program.cs
@@ -5,20 +5,27 @@ namespace Ex26_19_Takahashi
     {
         static void Main(string[] args)
         {
-                Box box = new Box(
-                    InputUtility.InputFloat("幅"),
-                    InputUtility.InputFloat("高さ"),
-                    InputUtility.InputFloat("奥行"));
-                
-                /*
-                 * 別バージョン
-                var width = InputUtility.InputFloat("幅");
-                var height = InputUtility.InputFloat("高さ");
-                var depth = InputUtility.InputFloat("奥行");
-                Box box = new Box(width, height, depth);
-                */
+            Box box = new Box(
+                InputUtility.InputFloat("幅"),
+                InputUtility.InputFloat("高さ"),
+                InputUtility.InputFloat("奥行"));
 
-                Console.WriteLine($"Boxの表面積={box.GetSurface()}\nBoxの体積={box.GetVlueme()}");
+            Cylinder cylinder = new Cylinder(
+                InputUtility.InputFloat("円の半径"),
+                InputUtility.InputFloat("円柱の高さ"));
+
+            Sphere sphere = new Sphere(
+                InputUtility.InputFloat("球の半径"));
+
+            TriangularPrism triangularprism = new TriangularPrism(
+                InputUtility.InputFloat("底辺"),
+                InputUtility.InputFloat("三角形の高さ"),
+                InputUtility.InputFloat("高さ"));
+
+            Console.WriteLine($"直方体の表面積={box.GetSurface()}\n直方体の体積={box.GetVlueme()}");
+            Console.WriteLine($"円柱の表面積={cylinder.GetSurface()}\n円柱の体積={cylinder.GetVlueme()}");
+            Console.WriteLine($"球の表面積={sphere.GetSurface()}\n球の体積={sphere.GetVlueme()}");
+            Console.WriteLine($"三角柱の表面積={triangularprism.GetSurface()}\n三角柱の体積={triangularprism.GetVlueme()}");
 
         }
     }
@@ -47,4 +54,73 @@ namespace Ex26_19_Takahashi
             return width * height * depth;
         }
     }
+
+    class Cylinder
+    {
+        float radius;
+        float height;
+
+        public Cylinder(float radius, float height)
+        {
+            this.radius = radius;
+            this.height = height;
+        }
+
+        public float GetSurface()
+        {
+            return radius * radius * (float)Math.PI * 2 + radius * 2 * (float)Math.PI * height;
+        }
+
+        public float GetVlueme()
+        {
+            return radius * radius * (float)Math.PI * height;
+        }
+    }
+
+    class Sphere
+    {
+        float radius;
+
+        public Sphere(float radius)
+        {
+            this.radius = radius;
+        }
+
+        public float GetSurface()
+        {
+            return radius * radius * (float)Math.PI * 4;
+        }
+
+        public float GetVlueme()
+        {
+            return 0.75f * (float)Math.PI * radius * radius * radius;
+        }
+    }
+
+    class TriangularPrism
+    {
+        float bottom;
+        float triangleHeigth;
+        float height;
+
+        public TriangularPrism(float bottom, float triangleHeight, float height)
+        {
+            this.bottom = bottom;
+            this.triangleHeigth = triangleHeight;
+            this.height = height;
+        }
+
+        public float GetSurface()
+        {
+            return bottom * triangleHeigth + (bottom + triangleHeigth + (float)Math.Sqrt(bottom * bottom + triangleHeigth * triangleHeigth)) * height;
+        }
+
+        public float GetVlueme()
+        {
+            return bottom * triangleHeigth / 2 * height;
+        }
+
+    }
+
 }
+
